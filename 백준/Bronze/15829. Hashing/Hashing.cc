01972a4ec87a57852f0c7a0e2e154c0e71bbdbde
Sanghoon Han <54487878+hsh200315@users.noreply.github.com>
2023-03-05 17:24:29 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,24 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 //15829
 
+#define M 1234567891
 
 int main(void)
 {
-	int N;
-	scanf("%d",&N);
+	int len;
+	long long sum = 0, R = 1;
 	char a[51];
+	
+	scanf("%d",&len);
 	scanf("%s",a);
-	int R = 1;
 	int num;
-	long long sum = 0;
-	for(int i = 0; i < N; i++)
+	for(int i = 0; i < len; i++)
 	{
-		num = (int)(a[i]);
-		num -= 'a' -1;
-		sum = (sum + num * R)%1234567891;
-		R = R*31;
+		sum = (sum + (a[i] -'a' + 1) * R) % M;
+		R = (R*31) % M;
 	}
 	printf("%lld\n",sum);
 }
