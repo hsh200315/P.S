7bee787e8d27f8b5138668b48888a5372eb6d2c0
Sanghoon Han <hsh200315@naver.com>
2024-04-01 11:08:55 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A,B,V;
+    int day = 0;
+    cin >> A >> B >> V;
+    if((V-A)%(A-B) == 0){
+        day = (V-A)/(A-B);
+    }
+    else{
+        day = (V-A)/(A-B) + 1;
+    }
+    cout << day+1;
+    
+}
