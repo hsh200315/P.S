9104ef244cddab7b9b2bea65bcf29bd54dad2415
Sanghoon Han <hsh200315@naver.com>
2024-04-01 10:29:21 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int N, M;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	cin >> M;
+
+	cout << N+M << '\n';
+	return 0;
+}
