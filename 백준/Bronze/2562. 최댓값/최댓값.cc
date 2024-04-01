8645de1b514660354754ec652d4a261d0f975bc4
Sanghoon Han <hsh200315@naver.com>
2024-04-01 11:11:53 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int max,index;
+	int arr[10];
+	for(int i=0; i<9; i++){
+		cin >> arr[i];
+	}
+	max = arr[0];
+	index = 0;
+	for(int i=1; i<9; i++){
+		if(max<arr[i]){
+			max = arr[i];
+			index = i;
+		}
+	}
+	cout << max << endl;
+	cout << index+1;
+}	
