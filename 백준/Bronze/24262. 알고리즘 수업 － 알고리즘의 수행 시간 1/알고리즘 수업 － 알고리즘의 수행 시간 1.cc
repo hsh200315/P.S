df3e6fa663e0d02afc4de89ce9dcfc2730d29b65
Sanghoon Han <hsh200315@naver.com>
2024-05-16 11:26:48 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    cout << "1\n";
+    cout << "0\n";
+    return 0;
+}
