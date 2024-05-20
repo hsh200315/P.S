32a9de4a8eb149b0a34910175d0ca3ff998e2002
Sanghoon Han <hsh200315@naver.com>
2024-05-20 20:28:42 +0900

    [Bronze V] Title: WARBOY, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int A,B,C;
+	cin >> A >> B >> C;
+	cout << B/A*C*3 << endl;
+}
